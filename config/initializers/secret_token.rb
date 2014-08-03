59fc75e4b352d36b5ecd59aceeb5a4cafe446d43
Carlos Sirias <arnaldoporras@gmail.com>
2014-08-03 15:23:21 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arn4ldo::Application.config.secret_key_base = '895eee2d2c6e52ecdbe3500656f833ef486ab6bb48922ab098b6322c659ac1c177347b7d7215449b6ba14ed6a6174c55a10f3aa9273b8c9decfa998c33fd78f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Arn4ldo::Application.config.session_store :cookie_store, key: '_arn4ldo_session'
