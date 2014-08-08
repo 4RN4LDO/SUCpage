9f408f159fc16b456f05f31f2911a73b2c7fd82e
Carlos Sirias <arnaldoporras@gmail.com>
2014-08-07 20:10:17 -0600

    Added Devise set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Arn4ldo::Application.routes.draw do
+  devise_for :users
   root :to => 'main#index'
 
   get 'home', to: "main#index", as: 'index'
+  get 'registrarse', to: "login#new", as: 'login'
 end
